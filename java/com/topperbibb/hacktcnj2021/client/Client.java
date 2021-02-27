b1a06bcbe8842b18855c847f65eff6325f5c114f
xaridar <xaridara@gmail.com>
2021-02-27 12:06:28 -0500

    add res

--- java/com/topperbibb/hacktcnj2021/client/Client.java
+++ java/com/topperbibb/hacktcnj2021/client/Client.java
@@ -1,7 +1,15 @@
 package com.topperbibb.hacktcnj2021.client;
 
+import java.io.IOException;
+import java.util.Arrays;
+
 public class Client {
   public static void main(String[] args) {
     System.out.println("HELLO");
+    try {
+      System.out.println(Arrays.toString(Client.class.getClassLoader().getResourceAsStream("text.txt").readAllBytes()));
+    } catch (IOException | NullPointerException e) {
+      e.printStackTrace();
+    }
   }
 }
