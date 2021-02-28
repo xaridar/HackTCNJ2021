5eaa0129e0bd4d56fbce3eac19428a0a1762ef95
xaridar <xaridara@gmail.com>
2021-02-27 22:37:23 -0500

    heroku jdk 11

--- src/main/java/com/topperbibb/hacktcnj2021/client/Main.java
+++ src/main/java/com/topperbibb/hacktcnj2021/client/Main.java
@@ -13,6 +13,6 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         User u = new User();
-        new Client("https://hacktcnj-2021.herokuapp.com/", 3000, u).connect();
+        new Client("hacktcnj-2021.herokuapp.com", 3000, u).connect();
     }
 }
