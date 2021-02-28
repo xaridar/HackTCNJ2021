86237be52c166242fe19abb1610c6504e049ea38
xaridar <xaridara@gmail.com>
2021-02-27 23:54:13 -0500

    fix env

--- src/main/java/com/topperbibb/hacktcnj2021/server/Main.java
+++ src/main/java/com/topperbibb/hacktcnj2021/server/Main.java
@@ -2,7 +2,7 @@ package com.topperbibb.hacktcnj2021.server;
 
 public class Main {
     public static void main(String[] args) {
-        String port = System.getenv("port");
+        String port = System.getenv("PORT");
         if (port == null) {
             port = "3000";
         }
